efd50e9353a103282e6355159b85b82001a4e1e2
athiyahnm <athiyahnazifah@gmail.com>
2021-10-19 08:22:55 +0700

    pertemuan 5

--- application/config/autoload.php
+++ application/config/autoload.php
@@ -60,7 +60,7 @@ $autoload['packages'] = array();
 |
 |	$autoload['libraries'] = array('user_agent' => 'ua');
 */
-$autoload['libraries'] = array('form_validation');
+$autoload['libraries'] = array('form_validation','session', 'database');
 
 /*
 | -------------------------------------------------------------------
@@ -91,7 +91,7 @@ $autoload['drivers'] = array();
 |
 |	$autoload['helper'] = array('url', 'file');
 */
-$autoload['helper'] = array('url','file');
+$autoload['helper'] = array('url');
 
 /*
 | -------------------------------------------------------------------
