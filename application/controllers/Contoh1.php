cd0009dc9f21909031000a8c5071db71c62d8cce
athiyahnm <athiyahnazifah@gmail.com>
2022-03-22 13:37:19 +0700

    All Pertemuan

---
+++ application/controllers/Contoh1.php
@@ -0,0 +1,12 @@
+<?php
+class Contoh1 extends CI_controller
+{
+    public function index()
+    {
+        echo "<h1>Perkenalkan</h1>";
+        echo"Nama saya Athiyah Nazifah
+            Saya tinggal di daerah Bekasi
+            olahraga yang saya sukai adalah
+            Bulutangkis";
+    }
+}
